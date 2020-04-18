8c51f79db17b55ce0960d7154efec52f0370b687
Faisal-havis <63634761+Faisal-havis@users.noreply.github.com>
2020-04-18 17:32:47 +0100

    Update dbconnect2.php

--- dbconnect2.php
+++ dbconnect2.php
@@ -3,7 +3,7 @@
 $servername = "eu-cdbr-west-03.cleardb.net";
 $username = "ba0024583dcfbd";
 $password = "60719ae6";
-$dbname = "heroku_81c3ead9329502b";
+$dbname = "airlines2";
 
 // Create connection
 $con = mysqli_connect($servername, $username, $password, $dbname);
