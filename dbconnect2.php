695d7676371d45099aa34f5fb128367ffcbf8d2d
Faisal-havis <63634761+Faisal-havis@users.noreply.github.com>
2020-04-18 17:36:14 +0100

    Update dbconnect2.php

--- dbconnect2.php
+++ dbconnect2.php
@@ -1,9 +1,9 @@
 <?php
 
-$servername = "eu-cdbr-west-03.cleardb.net";
-$username = "ba0024583dcfbd";
-$password = "60719ae6";
-$dbname = "airlines2";
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "airline2";
 
 // Create connection
 $con = mysqli_connect($servername, $username, $password, $dbname);
